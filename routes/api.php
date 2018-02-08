62a712a8dd6019576212643182b7e719eaf3f8ce
Ben Stoy <ben.stoy@noagent.co.uk>
2018-02-08 15:50:12 +0000

    route update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api as Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +12,9 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware('api')->namespace('Api')->group(function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::resource('properties', 'PropertyController', [
+        'only' => ['index', 'show', 'store']
+    ]);
 });
